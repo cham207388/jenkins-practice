833a9e0c1d629b1a689f55292f8b187e88b1fede
Alhagie Bai Cham <cham207388@gmail.com>
2020-11-23 18:35:27 -0800

    psuh

---
+++ src/main/java/com/abc/service/JenkinsService.java
@@ -0,0 +1,11 @@
+package com.abc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JenkinsService {
+
+    public String hello(){
+        return "Hello World Jenkins";
+    }
+}
---
+++ src/test/java/com/abc/service/JenkinsServiceTest.java
@@ -0,0 +1,15 @@
+package com.abc.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JenkinsServiceTest {
+
+
+    @Test
+    void hello() {
+        JenkinsService service = new JenkinsService();
+        assertEquals("Hello World Jenkins", service.hello());
+    }
+}
