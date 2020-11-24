ee6f8ae7aa95dc7b416847bb16e929a8737cdd16
Alhagie Bai Cham <cham207388@gmail.com>
2020-11-23 18:34:00 -0800

    push code to see if jenkins will trigger

--- src/main/java/com/abc/controller/JenkinsController.java
+++ src/main/java/com/abc/controller/JenkinsController.java
@@ -1,13 +1,18 @@
 package com.abc.controller;
 
+import com.abc.service.JenkinsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JenkinsController {
 
+    @Autowired
+    private JenkinsService jenkinsService;
+
     @GetMapping
     public String hello(){
-        return "Hello Jenkins";
+        return jenkinsService.hello();
     }
 }
