0ae0c5458c4ae3a40d8d95b04299415cf3183767
Alhagie Bai Cham <cham207388@gmail.com>
2020-11-25 19:26:16 -0800

    added jenkins file

--- src/test/java/com/abc/service/JenkinsServiceTest.java
+++ src/test/java/com/abc/service/JenkinsServiceTest.java
@@ -16,6 +16,6 @@ class JenkinsServiceTest {
     @Test
     void jenkins() {
         JenkinsService service = new JenkinsService();
-        assertEquals(2, service.jenkins());
+        assertEquals(1, service.jenkins());
     }
 }
